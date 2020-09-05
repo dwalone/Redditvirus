a4513cefc059f6f5b9fce984e33ef0628716f3a7
Rutwik <iamrutwik@gmail.com>
2020-09-05 21:25:47 +0100

    update

--- main.py
+++ main.py
@@ -1,22 +1,4 @@
 '''
-https://www.thelancet.com/journals/laninf/article/PIIS1473-3099(20)30243-7/fulltext#fig2
-
-onset to recovery - mean = 24.7, cv = 0.35
-to death - mean =18.8, cv =0.45
-
-my recovery - 7 days = 604800 so sd = 211680 (2.45 days)
-death - 5.3279... = 460334, sd = 207150 (2.40 days)
-
-since recovery after 0 days is unatural, keep 1 day minimum for recovery/death for both
-
-https://www.indexmundi.com/world/age_structure.html
-https://ourworldindata.org/mortality-risk-covid#case-fatality-rate-of-covid-19-by-age
-
-0-14 years: 25.29% (male 981,129,427/female 916,864,766)
-15-24 years: 15.77% (male 611,245,863/female 572,115,168)
-25-54 years: 41.03% (male 1,559,197,242/female 1,519,386,627)
-55-64 years: 8.84% (male 324,134,030/female 339,551,038)
-65 years and over: 9.06%
 
 make jupyter notebooks for data analysis
 
@@ -33,15 +15,12 @@ conn = sqlite3.connect('virus.db')
 c = conn.cursor()
 
 USERS = {}
-
 RECOVERY_TIME_MU = 604800
 RECOVERY_TIME_SD = 211680
 DEATH_TIME_MU = 460334
 DEATH_TIME_SD = 207150
-
 AGE_ELEMENTS = ["0-14", "15-24", "25-54", "55-64", "65+"]
 AGE_PROBABILITES = [0.2529, 0.1577, 0.4103, 0.0884, 0.0907] #65+ bumped from 0.0906 -> 0.0907 so probabilities sum to 1
-
 AGE_MORTALITY = {
                  "0-14" : 0.01,
                  "15-24" : 0.02,
@@ -51,7 +30,6 @@ AGE_MORTALITY = {
                  }
 
 IGNORE_USERS = ['AutoModerator', 'None']
-
 
 class user:
     
@@ -73,37 +51,29 @@ class user:
             self.uninfected_utc = int(max(34560, random.gauss(460334, 207150))) + infected_utc
             self.outcome = 'D'
             
-
-def initialinf():
-    
+def initialinf():        
     c.execute('''
               SELECT * FROM interactions
               ORDER BY created_utc ASC
               LIMIT 1
-              ''')
+              ''')     
               
-    row = c.fetchone()
-              
+    row = c.fetchone()              
     USERS[row[0]] = user(row[0], row[0], row[2])
-    
-    
-        
+            
 infcount = []
 utc = []        
 
-def main():
-    
-    count = 0
-    
-    initialinf()
+def main():    
+    count = 0    
+    initialinf()   
     
     c.execute('''SELECT * FROM interactions
                  ORDER BY created_utc ASC''')
                  
     for row in c:
         
-        interaction_utc = row[2]
-        
+        interaction_utc = row[2]        
         infcount.append(sum(1 for i in USERS.values() if i.uninfected_utc > interaction_utc))
         utc.append(interaction_utc)
         
@@ -124,17 +94,24 @@ def main():
             
                 if parent.uninfected_utc > interaction_utc:
                     USERS[authorname] = user(authorname, parentname, interaction_utc) 
-        count += 1
-        
+        count += 1        
         if count % 10000 == 0:
             print(count)
                     
-    #c.execute('''CREATE TABLE infections (id int, name text, infector int, infected_utc int, uninfected int, age text, outcome text)''')
-    #conn.commit()
-    #conn.close()
+    c.execute('''CREATE TABLE infections (id int, name text, infector text, infected_utc int, uninfected int, age text, outcome text)''')
+    for i in USERS.values():
+        datapoint = (int(i.id), str(i.name), str(i.infector), int(i.infected_utc), int(i.uninfected_utc), str(i.age), str(i.outcome))
+        c.execute('''INSERT INTO infections VALUES (?, ?, ?, ?, ?, ?, ?)'''
+                  , datapoint)
         
-        
-
+    c.execute('''CREATE TABLE infected_count (utc int, inf_count int)''')
+    for (i, j) in zip(utc, infcount):
+        datapoint = (int(i), int(j))
+        c.execute('''INSERT INTO infected_count VALUES (?, ?)'''
+                  , datapoint) 
+    
+    conn.commit()
+    conn.close()
+    
 if __name__ == "__main__":
     main()
-    
